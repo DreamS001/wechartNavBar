86f84fe344f6556586d8928ffc9405e9e821b84f
DreamS001 <1271486495@qq.com>
2019-07-11 12:00:26 +0800

    初始化

---
+++ components/header/header.js
@@ -0,0 +1,33 @@
+// components/header/header.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    statusBarHeight:{
+      type: String
+    },
+    headerTitle:{
+      type: String,
+      value: '首页'
+    },
+    bgColor:{
+      type: String,
+      value: 'blue'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
